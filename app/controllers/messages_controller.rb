50f07de822c5fc33a8e8581dd85dac515afe2351
seiyaokado <seiya.okado0323@gmail.com>
2019-05-15 19:14:16 +0900

    view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
+  
   def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
