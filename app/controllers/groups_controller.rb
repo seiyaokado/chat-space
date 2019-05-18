f36b83c3f18602819f07e81b299ddfb04abfffe4
seiyaokado <seiya.okado0323@gmail.com>
2019-05-18 18:05:19 +0900

    Message6
    部分テンプレートに修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,15 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
-
+  
+  def index
+    @groups = current_user.groups
+  end
+  
   def new
     @group = Group.new
     @group.users << current_user
   end
-
+  
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -14,7 +18,7 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-
+  
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -22,14 +26,16 @@ class GroupsController < ApplicationController
       render :edit
     end
   end
-
+  
   private
-
+  
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
-
+  
   def set_group
     @group = Group.find(params[:id])
   end
+  
+  
 end
